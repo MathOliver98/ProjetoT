225b09bf840e4145d38f455ee0f730d9ee5d510d
MathOliver98 <67709678+MathOliver98@users.noreply.github.com>
2022-09-28 13:12:55 -0300

    Update app.py

--- mainteste/app.py
+++ mainteste/app.py
@@ -14,29 +14,50 @@ mysql.init_app(app)
 
 @app.route('/')
 def main():
-    return render_template('index.html')
+    return render_template('cadastrar.html')
 
 @app.route('/gravar', methods=['POST','GET'])
 def gravar():
+  sqlQuery = """INSERT INTO tbl_produto (prod_marca, prod_nome, prod_preco, prod_qtd, prod_validade, prod_categoria, prod_ID) VALUES (%s, %s, %s, %s, %s, %s, %s)"""
   marca = request.form['marca']
   nome = request.form['nome']
   preco = request.form['preco']
   quantidade = request.form['quantidade']
   validade = request.form['validade']
   categoria = request.form['categoria']
-  if marca and nome and preco and quantidade and validade and categoria:
+  prodID = request.form['prodID']
+  inputDados = (marca, nome, preco, quantidade, validade, categoria, prodID)
+  if marca and nome and preco and quantidade and validade and categoria and prodID:
     conn = mysql.connect()
     cursor = conn.cursor()
-    cursor.execute('INSERT INTO tbl_produto (prod_marca, prod_nome, prod_preco, prod_qtd, prod_validade, prod_categoria) VALUES (%s, %s, %s, %s, %s, %s)', (marca, nome, preco, quantidade, validade, categoria))
+    cursor.execute(sqlQuery, inputDados)
     conn.commit()
   return render_template('cadastrar.html')
 
+@app.route('/alterar', methods=['PUT','GET'])
+def alterar():
+  sqlQuery = """UPDATE tbl_produto SET prod_marca = %s, prod_nome = %s, prod_preco = %s, prod_qtd = %s, prod_validade = %s, prod_categoria = %s WHERE prod_ID = %s"""
+  marca = request.form['marca']
+  nome = request.form['nome']
+  preco = request.form['preco']
+  quantidade = request.form['quantidade']
+  validade = request.form['validade']
+  categoria = request.form['categoria']
+  prodID = request.form['prodID']
+  inputDados = (marca, nome, preco, quantidade, validade, categoria, prodID)
+  if ProdID and marca and nome and preco and quantidade and validade and categoria:
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(sqlQuery, inputDados)
+    conn.commit()
+  return render_template('alterar.html')
 
 @app.route('/listar', methods=['POST','GET'])
 def listar():
+  sqlQuery = """SELECT prod_marca, prod_nome, prod_preco, prod_qtd, prod_validade, prod_categoria, prod_ID FROM tbl_produto"""
   conn = mysql.connect()
   cursor = conn.cursor()
-  cursor.execute('select produto_nome, produto_preco, produto_categoria from tbl_produto')
+  cursor.execute(sqlQuery)
   data = cursor.fetchall()
   conn.commit()
   return render_template('lista.html', datas=data)
