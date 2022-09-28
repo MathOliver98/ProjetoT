60943ee5ea1539fa6dfc23a1bb8957d43248bf4f
MathOliver98 <67709678+MathOliver98@users.noreply.github.com>
2022-09-28 10:36:50 -0300

    Add files via upload

---
+++ mainteste/app.py
@@ -0,0 +1,46 @@
+import os
+from flask import Flask, render_template, request
+from flaskext.mysql import MySQL
+
+mysql = MySQL()
+app = Flask(__name__)
+
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'admin'
+app.config['MYSQL_DATABASE_DB'] = 'teste'
+app.config['MYSQL_DATABASE_HOST'] = 'db'
+mysql.init_app(app)
+
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+@app.route('/gravar', methods=['POST','GET'])
+def gravar():
+  marca = request.form['marca']
+  nome = request.form['nome']
+  preco = request.form['preco']
+  quantidade = request.form['quantidade']
+  validade = request.form['validade']
+  categoria = request.form['categoria']
+  if marca and nome and preco and quantidade and validade and categoria:
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO tbl_produto (prod_marca, prod_nome, prod_preco, prod_qtd, prod_validade, prod_categoria) VALUES (%s, %s, %s, %s, %s, %s)', (marca, nome, preco, quantidade, validade, categoria))
+    conn.commit()
+  return render_template('cadastrar.html')
+
+
+@app.route('/listar', methods=['POST','GET'])
+def listar():
+  conn = mysql.connect()
+  cursor = conn.cursor()
+  cursor.execute('select produto_nome, produto_preco, produto_categoria from tbl_produto')
+  data = cursor.fetchall()
+  conn.commit()
+  return render_template('lista.html', datas=data)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5008))
+    app.run(host='0.0.0.0', port=port)
